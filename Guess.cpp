9935006ac1a8ac61f952b2e717b5c1aadd4d52c1
zisuan <lolkning3225@gmail.com>
2025-12-02 20:29:21 +0800

    debug

--- Guess.cpp
+++ Guess.cpp
@@ -8,23 +8,26 @@ int main() {
     cout << "=== Welcome to the Number Guessing Game! ===\n";
 
     srand(time(0));
-    int number = rand() % (MAX - MIN + MIN);
+    int number = rand() % (MAX - MIN + 1) + MIN;
 
-    int guess, attempts = 0;
+    int guess = -1;
+    int attempts = 0;
 
     while (guess != number) 
     {
         cout << "Guess a number between " << MIN << " and " << MAX << ": ";
         cin >> guess;
 
+        attempts++; // correct increment
+
         if (guess < number)
             cout << "Too low! Try again.\n";
         else if (guess > number)
             cout << "Too high! Try again.\n";
         else {
-            cout << "Congratulations! You guessed the number " << number << " in " << attempts << " attempts.\n";
+            cout << "Congratulations! You guessed the number " << number
+                 << " in " << attempts << " attempts.\n";
         }
-        attempts =+ 1;
     }
 
     cout << "---------------------------------\n";
